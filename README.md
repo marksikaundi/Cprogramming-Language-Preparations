bf3269844e198c999403b45391b463d3843a689a
marksikaundi <emmanuelmark117@gmail.com>
2022-05-28 23:34:56 +0200

    starting up with c

---
+++ README.md
@@ -0,0 +1,1 @@
+This the collection of my low level language c programming 
---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - print the phrase "programing is like building a multilingual puzzle
+ * return: 0 if exited properly, no-zero otherwise
+ */
+intmain(void)
+{
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
+}
