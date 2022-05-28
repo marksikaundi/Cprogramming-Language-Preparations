6fbfc278f869beee9ce857ab78c60b2117634331
marksikaundi <emmanuelmark117@gmail.com>
2022-05-29 00:01:47 +0200

    looking at c basics

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the phrase with proper grammar,
+ * but the output is a piece of art,
+ * Return: o if exited proper, non-zero otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
